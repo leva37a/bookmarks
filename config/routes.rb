2ea58bef89e21864ef3fc871a2bf6c21b1c1f79a
leva37a <leva37a@yandex.ru>
2023-02-07 12:35:31 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: 'bookmarks#index'
+
+  resources :bookmarks
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
